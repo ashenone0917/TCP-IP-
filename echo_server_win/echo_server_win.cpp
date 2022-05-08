33658c821950f0f1fc71fabc948768805cd5f3c1
ashen_one <ashenone0917@gmail.com>
2022-05-09 00:56:37 +0800

    echo

---
+++ echo_server_win/echo_server_win.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <WinSock2.h>
+
+#define BUF_SIZE 1024
+void errorHandle(char* msg);
+
+int main(int argc, char* argv[]) {
+
+}
+
+void errorHandle(char* msg) {
+
+}
